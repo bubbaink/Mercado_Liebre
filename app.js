5038205fab81b0658592173e1f81b55d31b3700e
Benitez Alexander <benitez.alexander.anibal@gmail.com>
2022-08-11 17:05:03 -0300

    formularios sin css

--- app.js
+++ app.js
@@ -8,4 +8,10 @@ app.use(express.static("public"))
 app.get("/", (req,res)=>{
     return res.sendFile(path.resolve(__dirname,"views","home.html"))
 })
+app.get("/register", (req,res)=>{
+    return res.sendFile(path.resolve(__dirname,"views","register.html"))
+})
+app.get("/login", (req,res)=>{
+    return res.sendFile(path.resolve(__dirname,"views","login.html"))
+})
 app.listen(port,()=> console.log(`Server ON http://localhost:${port}`));
